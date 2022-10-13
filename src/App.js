8b123f8d47cbd4c5935f5ed467294c5010abae0a
aat-traore <traoreadjiadiouma@gmail.com>
2022-10-13 15:48:56 +0000

    tprosp

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Lieu from './Component/Profile/Adress';
 import Names from './Component/Profile/FullName';
 import Pro from './Component/Profile/ProfilPhoto';
+import imge from './Component/Profile/assets/moi.jpeg';
 
 function App() {
   return (
     <>
-    <Pro/>
+    <Pro  fullName="Adji Adoiuma traore" bio="Depuis son retour à l’île " 
+    profession="developpeur" > ./assets/moi.jpeg </Pro>
     <Names/> 
     <Lieu/>
 
--- src/Component/Profile/ProfilPhoto.js
+++ src/Component/Profile/ProfilPhoto.js
@@ -1,10 +1,31 @@
 import React from "react";
-import imge  from './assets/moi.jpeg'
-const Pro = () =>{
+import PropTypes  from "prop-types";
+const Pro = (props) =>{
+    const handleName =() => {
+        alert(`My name is ${props.fullName}`)}
     return(
         <>
-        <img src={imge} className="imge" alt="photo"/>
+         {
+            handleName()
+         }
+         <div>
+            <img src={props.children} alt=" pict"/>
+         </div>
+        <div style={{backgroundColor: "black",width:300,color:"white", textAlign: "right",margintop:"0"}}>
+        <div > My fullName is :{props.fullName} </div>
+         <p> bio:{props.bio}</p>
+        <div>profession:{props.profession}</div>
+        </div>
+   
+    
         </>
     )
 }
+Pro.defaultProps = {
+    fullName: "Adji",
+    bio: "Adobe XD",
+    profession:"dev",
+    
+  }
+
 export default Pro;
